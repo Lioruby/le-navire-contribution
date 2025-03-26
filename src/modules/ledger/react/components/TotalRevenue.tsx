c43cacd07e5ee5c61748cd13429a6d91d8072afd
Lior Levy <levy.lior.pro@gmail.com>
2025-03-26 02:09:02 +0100

    chore: remove spelling mistake

--- src/modules/ledger/react/components/TotalRevenue.tsx
+++ src/modules/ledger/react/components/TotalRevenue.tsx
@@ -4,7 +4,7 @@ export const TotalRevenue = ({ totalRevenue }: { totalRevenue: number }) => {
   return (
     <div className="p-7 rounded-xl shadow-md bg-gray-100 w-full">
       <div className="flex items-center justify-between">
-        <h1 className="text-sm md:text-xl font-semibold">REVENUE TOTAL</h1>
+        <h1 className="text-sm md:text-xl font-semibold">REVENU TOTAL</h1>
         <div className="p-3 bg-blue-700 text-blue-100 rounded-full">
           <Wallet className="w-7 h-7" />
         </div>
